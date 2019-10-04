008063198a805609ac68b09376fa5e8b2e058c3b
luke3 <luke3@10.6.26.176>
2019-10-04 17:56:57 +0100

    LCA class made

--- DevelopmentTaskOne/src/LCA.java
+++ DevelopmentTaskOne/src/LCA.java
@@ -1,14 +1,48 @@
-
-public class LCA {
-
-	class Node {
+class Node {
 
 		int data;
 		Node left, right;
 
 		Node(int value) {
 			data = value;
-			left = right = null;
+			left = null;
+			right = null;
 		}
+	
+public class LCA {
+		
+		Node root;
+
+		Node findLCA(int node1, int node2) {
+
+			return findLCA(root, node1, node2);
+		}
+
+		Node findLCA(Node node, int node1, int node2) {
+
+			if (node == null) {
+				return null;
+			}
+
+			if (node.data == node1 || node.data == node2) {
+				return node;
+			}
+
+			Node left = findLCA(node.left, node1, node2);
+			Node right = findLCA(node.right, node1, node2);
+
+			if (left != null && right != null) {
+				return node;
+			}
+
+			if (left != null) {
+				return left;
+			} else {
+				return right;
+			}
+
+		}
+
+		
 	}
 }
