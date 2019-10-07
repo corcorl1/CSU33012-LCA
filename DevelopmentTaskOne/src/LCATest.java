d6bd017632760f9d602bf16f21aa65eb6b84b4e4
luke3 <luke3@LukesLaptop.Home>
2019-10-07 21:09:34 +0100

    Added test to test two nodes

--- DevelopmentTaskOne/src/LCATest.java
+++ DevelopmentTaskOne/src/LCATest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
 public class LCATest {
 
 	@Test
@@ -23,6 +24,16 @@ public class LCATest {
 		assertNull("Find LCA when tree is empty: ",isTreeEmpty.findLCA(1, 2));
 		assertEquals("Find LCA when tree is empty: ", null,isTreeEmpty.findLCA(1, 2));
 
+	}
+	
+	@Test
+	public void testTwoNodes(){
+
+		LCA tree = new LCA();
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+
+		assertEquals("Find LCA of tree with only one root and one child: ", 1, tree.findLCA(1, 2));	
 	}
 }
 
